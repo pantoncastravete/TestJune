2a7030050708a6f1e7acc291f16304aa04f76369
pantoncastravete <49492716+pantoncastravete@users.noreply.github.com>
2019-06-04 06:41:37 +0300

    Add files via upload

--- TestClock.js
+++ TestClock.js
@@ -1,58 +1,72 @@
 function showClock(hours, minutes, seconds) {
 	var secondsOutput = '';
-	var minutesOutput = '';
-	var hoursOutputLine1 = 'SSSS';
-	var hoursOutputLine2 = 'SSSS';
+	var minutesOutputLine1 = '';
+	var minutesOutputLine2 = '';
+	var hoursOutputLine1 = '';
+	var hoursOutputLine2 = '';
 	var isOddSecond = seconds % 2 === 1;
 
+	var fiveHoursLightsOn = Math.floor(hours / 5);
+	var oneHourLightsOn = hours % 5;
+
+	var fiveMinutesLightsOn = Math.floor(minutes / 5);
+	var oneMinuteLightsOn = minutes % 5;
+
 	for (var i = 0; i <= seconds; i++) {
-		if (isOddSecond) {
-			secondsOutput += i % 2 === 1 ? 'I' : '';
+		if (isOddSecond && i % 2 === 1) {
+			secondsOutput += 'I';
+		} else if (!isOddSecond && i % 2 === 0) {
+			secondsOutput += 'P';
+		}
+	}
+
+	for (var i = 0; i < 4; i++) {
+		if (fiveHoursLightsOn > 0) {
+			hoursOutputLine1 += 'A';
 		} else {
-			secondsOutput += i % 2 === 1 ? '' : 'P';
+			hoursOutputLine1 += 'S';
 		}
+
+		fiveHoursLightsOn--;
 	}
 
-	for (var i = 0; i <= Math.floor(hours / 5); i++) {
-		hoursOutputLine1 = hoursOutputLine1
-			.split('')
-			.splice(i, 1, 'A')
-			.join();
+	for (var i = 0; i < 4; i++) {
+		if (oneHourLightsOn > 0) {
+			hoursOutputLine2 += 'A';
+		} else {
+			hoursOutputLine2 += 'S';
+		}
+
+		oneHourLightsOn--;
 	}
 
-	if (hours > 20) {
-		for (var i = 0; i <= hours - 20; i++) {
-			hoursOutputLine2 = hoursOutputLine2
-				.split('')
-				.splice(i, 1, 'A')
-				.join();
+	for (var i = 0; i < 11; i++) {
+		if (fiveMinutesLightsOn > 0) {
+			minutesOutputLine1 += 'B';
+		} else {
+			minutesOutputLine1 += 'C';
 		}
+
+		fiveMinutesLightsOn--;
+	}
+
+	for (var i = 0; i < 4; i++) {
+		if (oneMinuteLightsOn > 0) {
+			minutesOutputLine2 += 'B';
+		} else {
+			minutesOutputLine2 += 'C';
+		}
+
+		oneMinuteLightsOn--;
 	}
 
 	console.log(isOddSecond ? 'S' : 'A');
 	console.log(hoursOutputLine1);
 	console.log(hoursOutputLine2);
+	console.log(minutesOutputLine1);
+	console.log(minutesOutputLine2);
 
 	console.log(secondsOutput);
 }
 
-showClock(21, 0, 21);
-script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js";
-
-$('[id^=h]').addClass('off');
-$('[id^=m]').addClass('off');
-
-if (fiveMinutes > 0) {
-	for (var i = 1; i <= fiveMinutes; i++) {
-		if (i % 3 == 0) {
-			$('#m5-' + i).removeClass('off');
-		} else {
-			$('#m5-' + i).removeClass('off');
-		}
-	}
-}
-if (singleMinutes > 0) {
-	for (var i = 1; i <= singleMinutes; i++) {
-		$('#m1-' + i).removeClass('off');
-	}
-}
+showClock(21, 21, 21);
